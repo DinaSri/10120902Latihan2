b52b93cb987bf63ed0036d50542a7474b7eddaa2
Dina Sri Hartini <dinahartini01@gmail.com>
2023-04-30 05:41:27 +0700

    Latihan 2-BLOODSHARE

---
+++ app/src/main/java/com/example/lat2_if10_10120902_dinasrihartini/HomeActivity.kt
@@ -0,0 +1,17 @@
+//29 April 2023
+//Latihan Bloodshare
+//10120902
+//Dina Sri Hartini
+//IF-10
+
+package com.example.lat2_if10_10120902_dinasrihartini
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
